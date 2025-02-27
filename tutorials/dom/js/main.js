4b8eb54b6c393ee06de9ddb6aedd827fcebfbf78
Padillaux <samantha.padilla01@sjsu.edu>
2025-02-26 16:47:04 -0800

    05: Dom + JavaScript Interaction
    Added divided images

---
+++ tutorials/dom/js/main.js
@@ -0,0 +1,114 @@
+
+document.querySelector("#artboard_1").addEventListener("click", function(){
+    document.querySelector("#artboard_2").style.visibility = "visible";
+    alert("bark bark bark");
+})
+
+document.querySelector("#artboard_2").addEventListener("click", function(){
+    document.querySelector("#artboard_3").style.visibility = "visible";
+    
+})
+
+document.querySelector("#artboard_3").addEventListener("click", function(){
+    document.querySelector("#artboard_4").style.visibility = "visible";
+    
+})
+
+document.querySelector("#artboard_4").addEventListener("click", function(){
+    document.querySelector("#artboard_5").style.visibility = "visible";
+    
+})
+
+document.querySelector("#artboard_5").addEventListener("click", function(){
+    document.querySelector("#artboard_6").style.visibility = "visible";
+    
+})
+
+document.querySelector("#artboard_6").addEventListener("click", function(){
+    document.querySelector("#artboard_7").style.visibility = "visible";
+    
+})
+
+document.querySelector("#artboard_7").addEventListener("click", function(){
+    document.querySelector("#artboard_8").style.visibility = "visible";
+    
+})
+
+document.querySelector("#artboard_8").addEventListener("click", function(){
+    document.querySelector("#artboard_9").style.visibility = "visible";
+    
+})
+
+document.querySelector("#artboard_9").addEventListener("click", function(){
+    document.querySelector("#artboard_1").style.visibility = "visible";
+    
+})
+// document.querySelector("#finalp").addEventListener("click", function(){
+//     document.querySelector("#frontimage").style.visibility = "visible";
+//     alert("bark bark bark");
+// })
+
+// document.querySelector("#frontimage").addEventListener("click", function(){
+//     document.querySelector("#futuro").style.visibility = "visible";
+// })
+
+// document.querySelector("#futuro").addEventListener("click", function(){
+//     document.querySelector("#finalp").style.visibility = "visible";
+// })
+
+
+/* you can remove the image completely
+ 
+ -making a change to the format of the website
+
+-the change in the code is 
+    .style.display = "none"
+
+    document.querySelector("#finalp").addEventListener("click", function(){
+    document.querySelector("#finalp").style.display = "none";
+
+*/
+
+
+
+
+
+
+
+
+
+/* Timeout and clickable tutorial
+console.log("Hello hello");
+
+let pageTitle = document.querySelector("#page-title")
+
+
+// Javascript Timeout changes h1 title after 3 sex
+setTimeout( function(){
+    pageTitle.style.color = "pink"; 
+    //console.log("timeout worked!")
+}   , 3000)
+
+//Click event on header changes background color
+document.querySelector("header").onclick = function() {
+    //console.log("clicked header");
+    document.querySelector("body").style.backgroundColor = "black";
+}
+
+
+/*Chaning without storing in variables
+
+// Javascript Timeout changes h1 title after 3 sex
+setTimeout( function(){
+    document.querySelector("#page-title").style.color = "pink"; 
+    //console.log("timeout worked!")
+}   , 3000)
+
+//Click event on header changes background color
+document.querySelector("header").onclick = function() {
+    //console.log("clicked header");
+    document.querySelector("body").style.backgroundColor = "black";
+}
+ */
+
+
