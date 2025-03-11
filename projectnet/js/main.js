39435d156ad0b7691f14368001d11643341cdec7
Padillaux <samantha.padilla01@sjsu.edu>
2025-03-11 14:53:35 -0700

    Project Net

---
+++ projectnet/js/main.js
@@ -0,0 +1,26 @@
+
+
+document.querySelector("#side_menu").addEventListener("click", function(){ alert("Tell me the truth.... How many times have you depended on ChatGBT when you really didn't need it? ");
+})
+
+document.querySelector("#text").addEventListener("click", function(){ alert("Ask yourself... Do you really need ChatGBT for this assignment? ");
+})
+
+document.querySelector("#send_quit").addEventListener("click", function(){ alert("Think about it. Is this a really professional text? ");
+})
+
+document.querySelector("#quit_job").addEventListener("click", function(){ alert("Again think about it. I know there is a lot going on but can you afford to quit your job? ");
+})
+
+document.querySelector("#shower").addEventListener("click", function(){ alert("Fine!! Since you want to use ChatGBT soo badly I will answer this question!");
+})
+
+document.querySelector("#shower").addEventListener("click", function(){
+    document.querySelector("#final_text").style.visibility = "visible";
+    alert("Fine!! Since you want to use ChatGBT soo badly I will answer this question!");
+})
+
+// document.querySelector("#shower").addEventListener("click", function(){
+//     document.querySelector("#final_text").style.visibility = "visible";
+//     alert("Fine!! Since you want to use ChatGBT soo badly I will answer this question!");
+// })
