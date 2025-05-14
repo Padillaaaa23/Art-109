ffa0edb9bc3068e0b9e108fba5e40e285d3603f1
Padillaux <samantha.padilla01@sjsu.edu>
2025-05-13 18:11:57 -0700

    JS sketch2

--- personal-site-p5/js/sketch.js
+++ personal-site-p5/js/sketch.js
@@ -1,4 +1,20 @@
+
+let canvas;
+
 function setup(){
-    createCanvas(400, 400);
-    background(225);
+    canvas = createCanvas(windowWidth, windowHeight);
+    canvas.position(0, 0);
+    // canvas.style("z-index", -2);
+    // background(225);
+}
+
+function windowResized(){
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+function draw(){
+
+    strokeWeight(0);
+    fill(random(200, 255), random(200, 255), random(200, 255));
+    ellipse(mouseX, mouseY, 30, 30)
 }
